6704c35fa080b653d18ad566ce87ea048f8f38a1
Navaneet <sainavaneet76@gmail.com>
2025-04-10 16:56:05 +0900

    update

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from omegaconf import DictConfig, OmegaConf
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def my_app(cfg : DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
+    #test
 
 if __name__ == "__main__":
     my_app()
